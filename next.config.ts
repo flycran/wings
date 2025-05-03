0b7b3e9a3ee25d9ee50214865aa2913567f04fee
33431 <cffhidol@163.com>
2025-05-04 01:48:57 +0800

    配置biome

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createMDX from '@next/mdx'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: [ 'js', 'jsx', 'md', 'mdx', 'ts', 'tsx' ],
   images: {
     remotePatterns: [],
   },
